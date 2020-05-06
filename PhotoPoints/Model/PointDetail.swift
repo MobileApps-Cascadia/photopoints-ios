284c863331666423002449018d3c77f127acca51
Clay Suttner <csuttner@gmail.com>
2020-05-06 13:43:54 -0700

    Adds PointDetail base class

---
+++ PhotoPoints/Model/PointDetail.swift
@@ -0,0 +1,11 @@
+//
+//  PointDetails.swift
+//  PhotoPoints
+//
+//  Created by Clay Suttner on 5/6/20.
+//  Copyright © 2020 Cascadia College. All rights reserved.
+//
+
+import Foundation
+
+class PointDetail
