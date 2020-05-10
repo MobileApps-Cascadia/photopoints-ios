224924a1fa91d7ca7f7ca17fb18b78bca10894bf
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-05-09 23:11:28 -0700

    Creates Realm database and implements database tests for object permanence and deletion

--- PhotoPoints/Model/Repository.swift
+++ PhotoPoints/Model/Repository.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 class Repository {
     
@@ -17,10 +18,46 @@ class Repository {
     // create a queue for operations requiring thread safety
     private let serialQueue = DispatchQueue(label: "repoQueue")
 
-    
-    
+    // instantiate the database. Creates a new one if none exists
+    private let realm:Realm = try! Realm()
+
+
     
     // TESTS
+    
+    
+    
+    // TODO:  REMOVE TEST CODE
+    // Repository Realm Test - A read/write test of Realm Database
+    // returns true of write, read, and delete of a test object succeeds
+    func testDB() -> Bool {
+
+        let results: String = "REPOSITORY REALM TEST:"
+        var success = true     // becomes false if any of the checks don't work
+
+        do {
+            let str = "TESTING"
+            let t = RealmTest()
+            
+            t.test = str
+            try realm.write {
+                realm.add(t)
+            }
+            var obj = realm.objects(RealmTest.self)
+            if obj.count == 0 {
+                success = false
+            } else {
+                if obj[0].test != str {success = false}
+            }
+            try realm.write{
+                realm.delete(obj)
+            }
+            obj = realm.objects(RealmTest.self)
+            if obj.count != 0 {success = false}
+        } catch {success = false}
+        print(results + (success ? "SUCCESS" : "FAIL"))
+        return success
+    }
     
     
     // TODO:  REMOVE TEST CODE
@@ -43,4 +80,10 @@ class Repository {
         print("REPOSITORY DEFAULT TEST: SUCCESS")
         return true
     }
+}
+
+// TODO:  REMOVE TEST CODE
+// Test class for Realm Database
+class RealmTest:Object {
+    @objc dynamic var test:String?
 }
