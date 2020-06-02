49dbdafc57e46875660ce0ab7414fd2a66c9a5ab
Clay Suttner <csuttner@gmail.com>
2020-06-01 23:33:31 -0700

    adds camera class

---
+++ PhotoPoints/Views/Scanner/CameraView.swift
@@ -0,0 +1,15 @@
+//
+//  CameraView.swift
+//  PhotoPoints
+//
+//  Created by Clay Suttner on 6/1/20.
+//  Copyright © 2020 Cascadia College. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraView: UIViewController {
+    
+}
+
