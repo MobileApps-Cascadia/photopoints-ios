10ae6187f8c1d4120a2b19547c074658b6288b12
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-06-07 13:49:27 -0700

    Fully comments the class

--- PhotoPoints/Models/Point.swift
+++ PhotoPoints/Models/Point.swift
@@ -2,11 +2,16 @@
 //  Point.swift
 //  PhotoPoints
 //
-//  Description:
 //  Whereas Item is the primary object in the data model, it is a composition of several
 //  different objects.  Point is the central object used by Item.  It contains the subset
 //  of data used by the functions of the Item class itself, those required by any part of
 //  the application using the Item.
+//
+//  Realm data fields
+//  id:        this is the primary key for parent "Item"; -1 represents invalid or unwritten object
+//  label:     a short string descriptor; used by the map or in lists where details are not accessed
+//  location:  the coordinates of the Item
+//  enabled:   boolean which determines if this point (and thus parent "Item" is returned in queries
 
 import Foundation
 import RealmSwift
@@ -30,7 +35,7 @@ class Point: Object {
 
 }
 
-// Standard initializer - use this during normal instantiation
+// Convenience initializer(s) - allows fields to be set upon instantiation
 extension Point {
     convenience init(id: Int, location: Coordinate, label: String, enabled: Bool) {
         self.init()
@@ -41,7 +46,8 @@ extension Point {
     }
 }
 
-// enable or disable this point (disables the item)
+// functions to enable or disable this point (disables the parent item)
+// not persistent until written to database
 extension Point {
     func enable() {
         enabled = true
