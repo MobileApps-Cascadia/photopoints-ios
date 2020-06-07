2a8a0e36ff8af3ac95bfe9dd411cead72ec44052
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-06-07 15:27:54 -0700

    qrInit: Replaces required init with convenience init and comments class

--- PhotoPoints/Models/QrLookup.swift
+++ PhotoPoints/Models/QrLookup.swift
@@ -2,21 +2,27 @@
 //  QrLookup.swift
 //  PhotoPoints
 //
-//  Created by Clay Suttner on 5/13/20.
-//  Copyright © 2020 Cascadia College. All rights reserved.
+//  Description:
+//  Provides a way to look up items by using a code, such as a QR code, a barcode,
+//  or some other string identifier
 //
+//  Realm data fields:
+//  qrCode:   The lookup string
+//  id:       The id of the associated item
+
 
 import Foundation
-import Realm
 import RealmSwift
 
 class QrLookup: Object {
-    @objc dynamic var id: Int = -1
-    @objc dynamic var qrCode: String = ""
+    @objc dynamic var id: Int = -1                // must be set to be valid
+    @objc dynamic var qrCode: String = "error"    // must be set to be valid
     
-    required init() {}
-    
-    required init(id: Int, qrCode: String) {
+}
+
+extension QrLookup {
+    convenience init(id: Int, qrCode: String) {
+        self.init()
         self.id = id
         self.qrCode = qrCode
     }
